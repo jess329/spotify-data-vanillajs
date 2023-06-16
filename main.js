2cdff959e2d1ba28e44ba68078f94178a41e0f4d
jasper <jasper.mittag@outlook.de>
2023-06-16 16:21:30 +0200

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+// Necessary information stored in variables
 const apiUrl = 'https://api.spotify.com/v1/browse/categories/toplists/playlists';
 const CLIENT_ID = "78e5f6cd10474053b7c867134e3f5205"
 const CLIENT_SECRET = "175b33f71a7e43d1b7a09156b2d26259"
@@ -8,8 +8,8 @@ let access_token = ""
 let refresh_token = ""
 const playlistId = "37i9dQZF1DX0XUsuxWHRQd"
 
-
-const fetchSpotifyCharts = async (token) => {
+// fetches some playlists from spotify
+const fetchSpotifyPlaylists = async (token) => {
     const resp = await fetch(apiUrl, {
           headers: {
             'Authorization': `Bearer ${token}`,
@@ -18,6 +18,8 @@ const fetchSpotifyCharts = async (token) => {
     const data = await resp.json()
     console.log(data);
 }
+
+// fetches data about a single playlist and calls the CreatePlaylistHTML() function with the data passed in
 const fetchPlaylist = async (token) => {
     const url = `https://api.spotify.com/v1/playlists/${playlistId}`
     const resp = await fetch(url, {
@@ -29,23 +31,45 @@ const fetchPlaylist = async (token) => {
     console.log(data);
     createPlaylistHMTL(data)
 }
+
+// creates the HTML structure for the playlist dynamically  
 const createPlaylistHMTL = (playlist) => {
-    const playlistDiv = document.getElementsByClassName('playlist')[0];
+    const songsDiv = document.getElementsByClassName("songs")[0]
+    const playlistHeader = document.getElementsByClassName("playlist-header")[0]
+    playlistHeader.innerText = `Playlist: ${playlist.name}`
+
+    // gets array of playlist songs and stores it in variable songs
     const songs = playlist.tracks.items
-    console.log(songs);
+    console.log(songs)
+
+    // maps over songs array and creates HTML for every song
     songs.map((song) => {
         const songDiv = document.createElement("div")
+        songDiv.classList.add("songDiv")
+
+        const songNameTag = document.createElement("h3")
         const song_name = document.createTextNode(`Track: ${song.track.name}`)
-        songDiv.appendChild(song_name)
-        playlistDiv.appendChild(songDiv)
+
+        songDiv.appendChild(songNameTag)
+        songNameTag.appendChild(song_name)
+        songsDiv.appendChild(songDiv)
+    
+        const imgholderDiv = document.createElement("div")
+        imgholderDiv.classList.add("imgholder")
+        // const songImgTag = `<img src=${song?.track?.album?.images?.images[0]?.url} classname="song-img" />`
+        // songDiv.appendChild(imgholderDiv)
     })
+        
 }
 
+// spreads the access token to the functions that fetch the actual spotify data
 const callDataFetching = (token) => {
-    fetchSpotifyCharts(token)
+    fetchSpotifyPlaylists(token)
     fetchPlaylist(token)
 }
 
+// fetches an access token from the spotify api when the site is loaded (valid for 1 hr)
+// returns the value that is returned by the callback function with the token passed in 
 const getToken = async (callback) => {
     const result = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
@@ -62,6 +86,7 @@ const getToken = async (callback) => {
     return callback(token)
 } 
 
+// Callback function, that also calls the CallDataFetching() function
 const assignAccessToken = (token) => {
     access_token = token;
     console.log(access_token);
@@ -72,9 +97,6 @@ access_token = getToken(assignAccessToken).then()
 console.log(access_token);
 
 
-fetchSpotifyCharts(access_token)
+// fetchSpotifyCharts(access_token)
 
-// const getAccessToken = async () => {
-//     access_token = await getToken()
-// }
 
